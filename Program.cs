7fc4a251e61915294e2a87ff9129aaf3077853fa
Jon Finerty <jonathanfinerty@gmail.com>
2022-02-11 09:17:48 +0000

    dotnet new

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var word = new Word();
+
+public class Word {
+    public List<Syllable> syllables;
+}
+
+public class Syllable {
+    public string text;
+    public bool isStressed;
+
+    public override string ToString()
+    {
+        return isStressed ? text.ToUpper() : text;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # ode
+
+Poetry IDE
+
+/ Poetry language
+
+feature ideas
+
+1 - detect rhyming scheme?
+2 - autocomplete rhymes
+3 - Online editor
+
+
+based on the book
+
+- metre
+	- iambic pentametre
+
+
+words broken up into syllables, with accented syllables
+
