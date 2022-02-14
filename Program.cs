cb458fdb9e485799da7ec918d46128548a88dc22
Jon Finerty <jonathanfinerty@gmail.com>
2022-02-14 17:51:40 +0000

    more rhymes

--- Program.cs
+++ Program.cs
@@ -6,104 +6,15 @@ using WikiClientLibrary.Generators;
 using WikiClientLibrary.Pages;
 using WikiClientLibrary.Sites;
 
-const int maxSyllableCount = 12;
-
 //csvToJs();
-SyllableCsvPurify();
+//SyllableCsvPurify();
 //DownloadWord().Wait();
-//var words = loadWords();
-//GetRhymes(words).Wait();
-// processRhymes();
+// var words = loadWords();
+// GetRhymes(words).Wait();
+processRhymes();
 // assignRhymeGroups();
 return;
 //DownloadAllWords().Wait();
-
-static void SyllableCsvPurify() {
-    var exit = false;
-    // at this point, lets get rid of dupes
-    List<Word> words = new List<Word>();
-    List<string> dedupedLines = new List<string>();
-    var processedLines = File.ReadAllLines("syllables2.csv");
-    var previousLine = processedLines.Last();
-    Word previousWord = new Word(previousLine);
-    
-    var caughtUp = false;
-    foreach (string line in File.ReadLines("syllables.csv"))
-    { 
-        if (!caughtUp) {
-            if (line == previousLine) {
-                caughtUp = true;
-            }
-            continue;
-        } 
-        
-        var components = line.Split(',');
-        var word = new Word(line);
-        Console.WriteLine(line);
-    
-        if (previousWord.text == word.text &&
-            previousWord.syllableCount == word.syllableCount && 
-            previousWord.primaryStressSyllableIndex == word.primaryStressSyllableIndex && 
-            previousWord.secondaryStressSyllableIndex == word.secondaryStressSyllableIndex) {
-            Console.WriteLine("Pure dupe, skipping");
-            dedupedLines.RemoveAt(dedupedLines.Count-1);
-            dedupedLines.Add(line);
-        } else if (previousWord.text == word.text) {
-            Console.WriteLine($"Duplicate found - {word.text}");
-            Console.WriteLine($"{previousWord.syllableCount}, {previousWord.primaryStressSyllableIndex}, {previousWord.secondaryStressSyllableIndex}        vs      {word.syllableCount}, {word.primaryStressSyllableIndex},{word.secondaryStressSyllableIndex}");
-            var resultChosen = false; 
-            var previousChosen = false;
-            while(!resultChosen) {
-                ConsoleKeyInfo _Key = Console.ReadKey();
-                switch (_Key.Key)
-                {
-                    case ConsoleKey.RightArrow:
-                        Console.WriteLine("Right Arrow");
-                        resultChosen = true;
-                        previousChosen = false;
-                        break;
-                    case ConsoleKey.LeftArrow:
-                        Console.WriteLine("Left Arrow");
-                        resultChosen = true;
-                        previousChosen = true;
-                        break;
-                    case ConsoleKey.Escape:
-                        Console.WriteLine("Exiting, writing to file");
-                        resultChosen = true;
-                        exit = true;
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            if (exit) {
-                using StreamWriter output = new($"syllables2.csv", append: true);
-                dedupedLines.RemoveAt(dedupedLines.Count-1);
-                foreach(var outputLine in dedupedLines) {
-                    output.WriteLine(outputLine);
-                }
-                break;
-            }
-
-            if (!previousChosen) {
-                dedupedLines.RemoveAt(dedupedLines.Count-1);
-                dedupedLines.Add(line);
-            }
-        } else {
-            dedupedLines.Add(line);
-        }
-
-        previousLine = line;
-        previousWord = word;
-    }
-
-    using StreamWriter output2 = new($"syllables2.csv", append: true);
-    dedupedLines.RemoveAt(dedupedLines.Count-1);
-    foreach(var outputLine in dedupedLines) {
-        output2.WriteLine(outputLine);
-    }
-}
 
 static void csvToJs() {
     // read syllables.csv
@@ -128,78 +39,15 @@ static void csvToJs() {
     output.WriteLine();
     output.WriteLine("let wordDict = {");
     foreach (var word in words) {
-        var props = $"[{word.Value.syllableCount},{word.Value.primaryStressSyllableIndex},{word.Value.secondaryStressSyllableIndex},[{String.Join(',',word.Value.rhymeGroups)}],";
+        var props = $"[{word.Value.syllableCount},{word.Value.primaryStressSyllableIndex},{word.Value.secondaryStressSyllableIndex},[{String.Join(',',word.Value.rhymeGroups)}]],";
         output.WriteLine($"\"{word.Value.text}\":" + props);
     }
     output.WriteLine("}");
 }
 
-static void processRhymes() {
-    List<List<string>> rhymeGroups = new List<List<string>>();
-    foreach (string line in File.ReadLines("rhymes.txt"))
-    {  
-        var group = line.Split(", ").Select(w => w.ToLower()).Distinct().ToList();
-        if (group.Count() > 1) {
-            group.Sort();
-            rhymeGroups.Add(group);
-            //Console.WriteLine(String.Join(", ",group));
-        }
-    }  
-
-    var distinct = rhymeGroups.Select(g => String.Join(",",g)).Distinct().ToList();
-    distinct.Sort();
-    // todo: remove ones which are proper suffixes of others
-    distinct = distinct.Where(d => !distinct.Any(superset => superset.Contains(d) && superset != d)).ToList();
-
-    rhymeGroups = distinct.Select(g => g.Split(",").ToList()).ToList();
-    using StreamWriter output = new($"rhymes_processed2.txt", append: true);
-
-    foreach (var group in rhymeGroups)
-    {  
-        var line = String.Join(", ",group);
-        output.WriteLine(line);
-    }  
-}
-
-static void assignRhymeGroups() {
-    var rhymeIndex = 0;
-    var words = loadWords();
-    foreach (string rhyme in File.ReadLines("rhymes_processed2.txt"))
-    {  
-        var rhymeWords = rhyme.Split(", ").ToList();
-        foreach(string rhymingWord in rhymeWords) {
-            var word = words.FirstOrDefault(w => w.text.ToLower() == rhymingWord);
-            if (word != null){
-                word.rhymeGroups.Add(rhymeIndex);
-            }
-        }
-
-        rhymeIndex++;
-    }  
-
-    foreach(var word in words) {
-        Console.WriteLine(word.text + " " + String.Join(",", word.rhymeGroups));
-    }
-
-    using StreamWriter output2 = new($"rhymes_processed3.txt", append: true);
-    foreach (string line in File.ReadLines("web/js/words.js"))
-    {
-        var parts = line.Split(":");
-        var word = parts[0].Replace("\"","");
-        var props = parts[1].Substring(0,parts[1].Length-2);
-        var wordData = words.FirstOrDefault(w => w.text.ToLower() == word);
-        if (wordData != null && wordData.rhymeGroups.Count() > 0) {
-            props = props + ",[" + String.Join(",", wordData.rhymeGroups) + "]],"; 
-        } else {
-            props = props + "],";
-        }
-        output2.WriteLine("\"" + word + "\":" + props);
-    }
-}
-
 static List<Word> loadWords() {
     var words = new List<Word>();
-    foreach (string line in File.ReadLines("words.txt"))
+    foreach (string line in File.ReadLines("syllables.csv"))
     {  
         var word = new Word(line);
         words.Add(word);
@@ -224,6 +72,34 @@ static string GetPronounciation(WikiPage page) {
     }
 }
 
+static void processRhymes() {
+    List<List<string>> rhymeGroups = new List<List<string>>();
+    foreach (string line in File.ReadLines("rhymes2.csv"))
+    {  
+        var group = line.Split(",").ToList();
+        if (group.Count() > 1) {
+            group.Sort();
+            rhymeGroups.Add(group);
+        }
+    }  
+
+    Console.WriteLine(rhymeGroups.Count());
+
+    var distinct = rhymeGroups.Select(g => String.Join(",",g)).Distinct().ToList();
+    distinct.Sort();
+
+    distinct = distinct.Where(d => !distinct.Any(superset => superset.Contains(d) && superset != d)).ToList();
+
+    rhymeGroups = distinct.Select(g => g.Split(",").ToList()).ToList();
+    using StreamWriter output = new($"rhymes_processed2.csv", append: false);
+
+    foreach (var group in rhymeGroups)
+    {  
+        var line = String.Join(",",group);
+        output.WriteLine(line);
+    }  
+}
+
 static List<string> GetRhymeSuffixes(string content) {
     var suffixes = new List<string>();
     
@@ -244,12 +120,14 @@ static List<string> GetRhymeSuffixes(string content) {
     return suffixes;
 }
 
-static async Task<List<string>> GetRhyme(WikiSite site, string rhyme, string previousRhymePage) {
+static async Task<List<string>> GetRhyme(WikiSite site, string rhyme, string? previousRhymePage, string? content) {
     var rhymingWords = new List<string>();
-    var pageRead = new WikiPage(site, "Rhymes:English/"+rhyme);
-    await pageRead.RefreshAsync(PageQueryOptions.FetchContent);
+    if (content == null) {
+        var pageRead = new WikiPage(site, "Rhymes:English/"+rhyme);
+        await pageRead.RefreshAsync(PageQueryOptions.FetchContent);
 
-    var content = pageRead.Content;
+        content = pageRead.Content;
+    }
     if (content == null) {
         return rhymingWords;
     }
@@ -258,7 +136,7 @@ static async Task<List<string>> GetRhyme(WikiSite site, string rhyme, string pre
     var redirectMatches = redirect.Matches(content);
     var firstMatch = redirectMatches.FirstOrDefault();
     if (firstMatch != null) { 
-        return await GetRhyme(site, firstMatch.Groups[1].Value, rhyme);
+        return await GetRhyme(site, firstMatch.Groups[1].Value, rhyme, null);
     }
 
     var seeAlso = new Regex("\\# See also \\[\\[Rhymes:English/(.+?)\\|");
@@ -268,7 +146,7 @@ static async Task<List<string>> GetRhyme(WikiSite site, string rhyme, string pre
         if (previousRhymePage == newRhymePage) {
             continue;
         }
-        var subrhymes = await GetRhyme(site, newRhymePage, rhyme);
+        var subrhymes = await GetRhyme(site, newRhymePage, rhyme, null);
         rhymingWords.AddRange(subrhymes);
     }
 
@@ -281,7 +159,7 @@ static async Task<List<string>> GetRhyme(WikiSite site, string rhyme, string pre
 
         // regex out subrhymeSuffixes
         foreach(var suffix in processedSuffixes) {
-            var subrhymes = await GetRhyme(site, newRhymePage, rhyme);
+            var subrhymes = await GetRhyme(site, newRhymePage, rhyme, null);
             subrhymes = subrhymes.Select(x => x + suffix).ToList();
             rhymingWords.AddRange(subrhymes);
         }
@@ -310,6 +188,13 @@ static async Task<List<string>> GetRhyme(WikiSite site, string rhyme, string pre
 }
 
 static async Task GetRhymes(List<Word> words) {
+    var wordsDict = new Dictionary<string, Word>();
+    foreach (string line in File.ReadLines("syllables.csv"))
+    { 
+        var word = new Word(line);
+        wordsDict.Add(word.text, word);
+    }
+
     var client = new WikiClient
     {
         ClientUserAgent = "WCLQuickStart/1.0 (your user name or contact information here)"
@@ -317,36 +202,40 @@ static async Task GetRhymes(List<Word> words) {
     var site = new WikiSite(client, "https://en.wiktionary.org/w/api.php");
     await site.Initialization;
 
-    var catmembers = new CategoryMembersGenerator(site, $"Category:English_rhymes")
+
+    var pageGen = new AllPagesGenerator(site)
     {
-        MemberTypes = CategoryMemberTypes.Page,
+        StartTitle = "English/aɪ",
+        NamespaceId = 106,
         PaginationSize = 50
     };
 
-    using StreamWriter file = new($"rhymes.txt", append: true);
+    var rhymepagecount = 0;
 
-    await using (var enumerator = catmembers.EnumPagesAsync(PageQueryOptions.FetchContent).GetAsyncEnumerator())
+    using StreamWriter output = new($"rhymes2.csv", append: false);
+
+    await using (var enumerator = pageGen.EnumPagesAsync(PageQueryOptions.FetchContent).GetAsyncEnumerator())
     {
-        await enumerator.MoveNextAsync(CancellationToken.None); //skip introduction
         while (await enumerator.MoveNextAsync(CancellationToken.None))
         {
             var page = enumerator.Current;
-            //Console.WriteLine(page.Content);
-            //[[Rhymes:English/a??ni|
-            var regex = new Regex("Rhymes:English/(.+?)\\|");
-            MatchCollection matches = regex.Matches(page.Content);
-            foreach (Match match in matches) {
-                // we are now in a distinct rhyme.
-                var rhymingWords = await GetRhyme(site, match.Groups[1].Value, null);
-                rhymingWords = rhymingWords.Where(w => words.Any(validWord => validWord.text == w)).Distinct().ToList();    
-                if (rhymingWords.Count >0 ){
-                    var line = String.Join(", ",rhymingWords);
-                    Console.WriteLine(line);
-                    await file.WriteLineAsync(line);
-                }
+            if (page.Title == "Rhymes:Esperanto/aa") {
+                Console.WriteLine("Stopping");
+                break;
             }
+            Console.WriteLine(page.Title);
+            var rhymes = await GetRhyme(site, page.Title, null, page.Content);
+            rhymepagecount++;
+            Console.WriteLine($"{rhymepagecount}/7599 complete");
+            rhymes = rhymes.Select(r => r.ToLower()).Distinct().Where(r => wordsDict.ContainsKey(r)).ToList();
+            if (rhymes.Count > 0) {
+                var outputLine = String.Join(",", rhymes);
+                output.WriteLine(outputLine);
+            }            
         }
     }
+
+    return;
 }
 
 static async Task DownloadWord() {
@@ -448,23 +337,6 @@ public class Word {
 
     public override string ToString()
     {
-        // var stressedSyllable = '-';//'●';
-        // var unstressedSyllable = '/';//'○';
-        // var sb = new StringBuilder();
-        // for (int i=0; i<syllableCount; i++) {
-        //     if (i!= 0) {
-        //         sb.Append(' ');
-        //     }
-        //     if (i == primaryStressSyllableIndex || i == secondaryStressSyllableIndex) {
-        //         sb.Append(stressedSyllable);
-        //     } else {
-        //         sb.Append(unstressedSyllable);
-        //     }
-        // }
-
-        // var padding = Math.Max(0, ((text.Length - sb.Length) / 2));
-
-        // return new String(' ', padding) + sb.ToString() + '\n' + text;
         return $"{text},{syllableCount},{primaryStressSyllableIndex},{secondaryStressSyllableIndex}";
     }
 
