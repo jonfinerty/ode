bce32cbea6b268257b8905208578b4e6e055f02e
Jon Finerty <jonathanfinerty@gmail.com>
2022-02-17 18:05:48 +0000

    improve word list pipeline

--- Program.cs
+++ Program.cs
@@ -1,26 +1,201 @@
-﻿using System.Text.RegularExpressions;
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Collections.Concurrent;
 
 HttpClient client = new HttpClient();
-
-//csvToJs();
-//SyllableCsvPurify();
-//DownloadWord().Wait();
-// var words = loadWords();
-// GetRhymes(words).Wait();
-// processRhymes();
-// processSyllables();
-// csvToJs();
-// assignRhymeGroups();
-//processWordList();
-//var rhymes = await getRhymesFromDatamuse(client, "fiddle");
-// foreach (var rhyme in rhymes) {
-//     Console.WriteLine(rhyme);
-// }
-downloadFromDatamuse(client).Wait();
+//sanitiseDatamuseRhymes();
+csvsToJs();
 return;
-//DownloadAllWords().Wait();
+
+
+static void sanitiseDatamuseRhymes() {
+    var datamuseRhymes = new List<List<string>>();
+    foreach(string line in File.ReadLines("datamuse_rhymes.csv")) {
+        var rhymingGroup = line.Split(',').ToList();
+        datamuseRhymes.Add(rhymingGroup);
+    }
+
+    for (var i =0; i<datamuseRhymes.Count; i++) {
+        for (var j=i+1; j<datamuseRhymes.Count; j++) {
+            foreach(var word in datamuseRhymes[i]) {
+                if (datamuseRhymes[j].Contains(word)) {
+                    Console.WriteLine("Dupe word: " +word);
+                }
+                break;
+            }
+        }
+    }
+}
+
+static void sanitiseDatamuseData() {
+    var words = LoadWords("datamuse_words.csv", true);
+    var orderedWords = words.Values.OrderBy(w => w.text).Where(w => !w.text.Contains('-')).ToList();
+    using (StreamWriter output = new($"datamuse_words2.csv", append: false))
+    {
+        foreach (var word in orderedWords) {
+            output.WriteLine(word.ToCSVFormat());
+        }
+    }
+}
+
+static Dictionary<string, Word> LoadWords(string filename, bool isDatamuseFile) {
+    var words = new Dictionary<string, Word>();
+    foreach (string line in File.ReadLines(filename))
+    { 
+        var word = new Word(line, isDatamuseFile);
+        words.TryAdd(word.text, word);
+    }
+
+    return words;
+}
+
+static HashSet<string> LoadUnaccentedParticles() {
+    var words = new HashSet<string>();
+    foreach (string line in File.ReadLines("one_syllable_articles.csv"))
+    { 
+        words.Add(line);
+    }
+    foreach (string line in File.ReadLines("one_syllable_conjuctions.csv"))
+    { 
+        words.Add(line);
+    }
+    foreach (string line in File.ReadLines("one_syllable_prepositions.csv"))
+    { 
+        words.Add(line);
+    }
+    foreach (string line in File.ReadLines("one_syllable_pronouns.csv"))
+    { 
+        words.Add(line);
+    }
+
+    return words;
+} 
+
+static void csvsToJs() {
+    var freqThreshold = 0;
+    var wordCountLimit = 100000;
+
+    var mergedWords = new Dictionary<string, Word>();
+
+    var unaccentedParticles = LoadUnaccentedParticles();
+    var wiktionaryWords = LoadWords("wiktionary_syllables.csv", false);
+    var datamuseWords = LoadWords("datamuse_words.csv", true);
+
+    // assume wiktionary word crawl is more correct
+    foreach(var word in wiktionaryWords) {
+        if (unaccentedParticles.Contains(word.Key)){
+            word.Value.SetAsUnaccentedParticle();
+        }
+        mergedWords.Add(word.Key, word.Value);
+    }
+
+    foreach(var word in datamuseWords) {
+        if (mergedWords.ContainsKey(word.Key)) {
+            // wiktionary words don't have IPA or Freq stored
+            mergedWords[word.Key].IPA = word.Value.IPA; 
+            mergedWords[word.Key].freqScore = word.Value.freqScore; 
+        } else if (word.Value.freqScore > freqThreshold) {
+            mergedWords.Add(word.Key, word.Value);
+        }
+    }
+
+    foreach(var particle in unaccentedParticles) {
+        mergedWords[particle].SetAsUnaccentedParticle();
+    }
+
+    mergedWords = mergedWords.OrderByDescending(w => w.Value.freqScore).Take(wordCountLimit).ToDictionary(x => x.Key, x => x.Value);
+
+    // assume datamuse rhyme data is better?
+    int rhymeIndex = 0;
+    var datamuseRhymes = new Dictionary<int, List<string>>();
+    foreach(string line in File.ReadLines("datamuse_rhymes.csv")) {
+        var rhymingGroup = line.Split(',').Where(r => !r.Contains("-")).ToList();
+        if (rhymingGroup.Count <= 1) {
+            continue;
+        }
+        datamuseRhymes.Add(rhymeIndex, rhymingGroup);
+        foreach(string word in rhymingGroup) {
+            // might not be in there due to freq threshold
+            if (mergedWords.ContainsKey(word)) {
+                mergedWords[word].rhymeGroups.Add(rhymeIndex);
+            }
+        }
+        rhymeIndex++;
+    }
+
+    // backfill any missing rhymes with wiktionary
+    var wiktionaryRhymes = new List<List<string>>();
+    foreach(string line in File.ReadLines("wiktionary_rhymes.csv")) {
+        var rhymingGroup = line.Split(',').ToList();
+        wiktionaryRhymes.Add(rhymingGroup);
+    }
+
+    foreach(var word in mergedWords) {
+        if (word.Value.rhymeGroups.Count == 0) {
+            var rhymeFound = false;
+
+            var wiktionaryRhymingGroups = wiktionaryRhymes.Where(r => r.Contains(word.Key));
+            foreach (var wiktionaryRhymingGroup in wiktionaryRhymingGroups) {
+                foreach (var wiktionaryRhymingWord in wiktionaryRhymingGroup) {
+                    // see if we have a datamuses group with this word if so, add unrhymed word to it
+                    foreach (var datamuseRhymingGroup in datamuseRhymes) {
+                        if (datamuseRhymingGroup.Value.Contains(wiktionaryRhymingWord)) {
+                            word.Value.rhymeGroups.Add(datamuseRhymingGroup.Key);
+                            rhymeFound = true;
+                            break;
+                        }
+                    }
+                    if (rhymeFound) {
+                        break;
+                    }
+                }
+                if (rhymeFound) {
+                    break;
+                }
+            }
+        }
+    }
+
+    using StreamWriter output = new($"docs/js/words.js", append: false);
+    output.WriteLine("\"use strict\";");
+    output.WriteLine();
+    output.WriteLine("let wordDict = {");
+    foreach (var word in mergedWords.OrderBy(w => w.Key)) {
+        var props = $"[{word.Value.syllableCount},{word.Value.primaryStressSyllableIndex},{word.Value.secondaryStressSyllableIndex},[{String.Join(',',word.Value.rhymeGroups)}]],";
+        output.WriteLine($"\"{word.Value.text}\":" + props);
+    }
+    output.WriteLine("}");
+
+    /*
+    // read syllables.csv
+    var words = new Dictionary<string, Word>();
+    foreach (string line in File.ReadLines("syllables.csv"))
+    { 
+        var word = new Word(line);
+        words.Add(word.text, word);
+    }
+    // read rhymes.csv
+    int rhymeIndex = 0;
+    foreach(string line in File.ReadLines("rhymes.csv")) {
+        var rhymingGroup = line.Split(',');
+        foreach(string word in rhymingGroup) {
+            words[word].rhymeGroups.Add(rhymeIndex);
+        }
+        rhymeIndex++;
+    }
+
+    using StreamWriter output = new($"docs/js/words.js", append: false);
+    output.WriteLine("\"use strict\";");
+    output.WriteLine();
+    output.WriteLine("let wordDict = {");
+    foreach (var word in words) {
+        var props = $"[{word.Value.syllableCount},{word.Value.primaryStressSyllableIndex},{word.Value.secondaryStressSyllableIndex},[{String.Join(',',word.Value.rhymeGroups)}]],";
+        output.WriteLine($"\"{word.Value.text}\":" + props);
+    }
+    output.WriteLine("}");
+    */
+}
+
+
 
 static async Task<Word> getWordFromDatamuse(HttpClient client, string text) {
     var path = "https://api.datamuse.com/words?sp="+text+"&md=sfr&max=1&ipa=1";
@@ -76,7 +251,7 @@ static async Task downloadFromDatamuse(HttpClient client) {
     var words = new ConcurrentDictionary<string, Word>();
     foreach (string line in File.ReadLines("datamuse_syllables.csv"))
     { 
-        var word = new Word(line);
+        var word = new Word(line, true);
         words.TryAdd(word.text, word);
     }
 
@@ -175,45 +350,6 @@ static void processWordList() {
     }
 }
 
-static void csvToJs() {
-    // read syllables.csv
-    var words = new Dictionary<string, Word>();
-    foreach (string line in File.ReadLines("syllables.csv"))
-    { 
-        var word = new Word(line);
-        words.Add(word.text, word);
-    }
-    // read rhymes.csv
-    int rhymeIndex = 0;
-    foreach(string line in File.ReadLines("rhymes.csv")) {
-        var rhymingGroup = line.Split(',');
-        foreach(string word in rhymingGroup) {
-            words[word].rhymeGroups.Add(rhymeIndex);
-        }
-        rhymeIndex++;
-    }
-
-    using StreamWriter output = new($"docs/js/words.js", append: false);
-    output.WriteLine("\"use strict\";");
-    output.WriteLine();
-    output.WriteLine("let wordDict = {");
-    foreach (var word in words) {
-        var props = $"[{word.Value.syllableCount},{word.Value.primaryStressSyllableIndex},{word.Value.secondaryStressSyllableIndex},[{String.Join(',',word.Value.rhymeGroups)}]],";
-        output.WriteLine($"\"{word.Value.text}\":" + props);
-    }
-    output.WriteLine("}");
-}
-
-static List<Word> loadWords() {
-    var words = new List<Word>();
-    foreach (string line in File.ReadLines("syllables.csv"))
-    {  
-        var word = new Word(line);
-        words.Add(word);
-    }  
-    return words;
-}
-
 static void processRhymes() {
     List<List<string>> rhymeGroups = new List<List<string>>();
     foreach (string line in File.ReadLines("rhymes.csv"))
@@ -242,51 +378,49 @@ static void processRhymes() {
     }  
 }
 
-static void processSyllables() {
-    var words = loadWords();
-
-    words = words.OrderBy(w => w.text).ToList();
-
-    using StreamWriter output = new($"syllables.csv", append: false);
-
-    foreach (var word in words)
-    {  
-        output.WriteLine(word.ToString());
-    }  
-}
-
 public class Word {
-    public string? text;
-    public string? IPA;
+    public string text;
+    public string IPA;
     public int syllableCount;
     public double freqScore; // number of times out a 1 million it appears in google book corpus
-    public int? primaryStressSyllableIndex;
-    public int? secondaryStressSyllableIndex;
+    public int primaryStressSyllableIndex;
+    public int secondaryStressSyllableIndex;
     public List<int> rhymeGroups = new List<int>();
     const char primaryStressSymbol = 'ˈ';
     const char secondaryStressSymbol = 'ˌ';
 
-    public Word(){}
-
-    public Word(string text, string IPA, int syllableCount, double freqScore) {
+    public Word(string text, string IPA, int syllableCount, double freqScore, bool isDatamuseWord) {
         this.text = text;
         this.IPA = IPA;
         this.syllableCount = syllableCount;
         this.freqScore = freqScore;
         primaryStressSyllableIndex = GetPrimaryStressSyllableIndex(syllableCount, IPA);
-        secondaryStressSyllableIndex = GetSecondaryStressSyllableIndex(syllableCount, IPA);
+        secondaryStressSyllableIndex = GetSecondaryStressSyllableIndex(syllableCount, IPA, isDatamuseWord);
     }
 
-    public Word(string fileLine) {
+    public Word(string fileLine, bool isDatamuseWord) {
         var sections = fileLine.Split(',');
         text = sections[0].ToLower();
         syllableCount = int.Parse(sections[1]);
+        if (isDatamuseWord) {
+            freqScore = double.Parse(sections[2]);
+            IPA = sections[3];
+            primaryStressSyllableIndex = GetPrimaryStressSyllableIndex(syllableCount, IPA);
+            secondaryStressSyllableIndex = GetSecondaryStressSyllableIndex(syllableCount, IPA, isDatamuseWord);
+            return;
+        }
+
+        // default freq score
+        freqScore = 1;
+
         if (sections[2].Length > 0) {
             primaryStressSyllableIndex = int.Parse(sections[2]);
             if (primaryStressSyllableIndex >= syllableCount) {
                 Console.WriteLine("Updating primary syllable stress within bounds");
                 primaryStressSyllableIndex = syllableCount-1;
             }
+        } else {
+            primaryStressSyllableIndex = -1;
         }
         if (sections[3].Length > 0) {
             secondaryStressSyllableIndex = int.Parse(sections[3]);
@@ -294,48 +428,77 @@ public class Word {
                 Console.WriteLine("Updating primary syllable stress within bounds");
                 primaryStressSyllableIndex = syllableCount-1;
             }
+        } else {
+            primaryStressSyllableIndex = -1;
         }
     }
 
-    public override string ToString()
-    {
-        return $"{text},{syllableCount},{primaryStressSyllableIndex},{secondaryStressSyllableIndex},{freqScore},{IPA}";
+    public void SetAsUnaccentedParticle() {
+        syllableCount = 1;
+        primaryStressSyllableIndex = -1;
+        secondaryStressSyllableIndex = -1;
     }
 
-    static int? GetPrimaryStressSyllableIndex(int syllableCount, string pronunciation) {
+    public string ToCSVFormat()
+    {
+        // stresses are recomputed each time
+        return $"{text},{syllableCount},{freqScore},{IPA}";
+    }
+
+    static int GetPrimaryStressSyllableIndex(int syllableCount, string pronunciation) {
         if (pronunciation == null) {
-            return null;
+            return -1;
+        }
+
+        if (pronunciation.Length <= 0) {
+            return -1;
         }
         var characterIndex = pronunciation.IndexOf(primaryStressSymbol);
         if (characterIndex == -1) {
-            return null;
+            return -1;
         }
 
         return EstimateSyllableIndex(syllableCount, pronunciation, characterIndex);
     }
 
-    static int? GetSecondaryStressSyllableIndex(int syllableCount, string pronunciation) {
+    static int GetSecondaryStressSyllableIndex(int syllableCount, string pronunciation, bool isDatamuseWord) {
         if (pronunciation == null) {
-            return null;
+            return -1;
         }
 
-        var characterIndex = pronunciation.IndexOf(secondaryStressSymbol);
+        if (pronunciation.Length <= 0) {
+            return -1;
+        }
+
+        var characterIndex = -1;
+
+        if (isDatamuseWord) {
+            var firstOccurance = pronunciation.IndexOf(primaryStressSymbol);
+            if (firstOccurance != -1) {
+                characterIndex = pronunciation.IndexOf(primaryStressSymbol, firstOccurance + 1);
+            }
+        } else {
+            characterIndex = pronunciation.IndexOf(secondaryStressSymbol);
+        }
+
         if (characterIndex == -1) {
-            return null;
+            return -1;
         }
 
         return EstimateSyllableIndex(syllableCount, pronunciation, characterIndex);
     }
 
+    // todo: improve
     static int EstimateSyllableIndex(int syllableCount, string pronunciation, int characterIndex) {
-        //Console.WriteLine($"{syllableCount} {characterIndex} {pronunciation} {pronunciation.Length}");
         if (characterIndex == 0) {
+            return 0;
+        }
+        if (syllableCount == 1) {
             return 0;
         }
 
         var result = Convert.ToInt32(((double) characterIndex / pronunciation.Length)*syllableCount);
-        // it can't be 0
-        return Math.Max(1, result);
+        return Math.Clamp(result,1,syllableCount-1);
     }
 
 }
@@ -359,7 +522,7 @@ public class DatamuseWord {
     }
 
     public Word ToWord() {
-        var word = new Word(this.word, GetIPA(), numSyllables, GetFreqScore());
+        var word = new Word(this.word, GetIPA(), numSyllables, GetFreqScore(), true);
         return word;
     }
 }
