53618a011b4c7ffa8108ddc1dcf02815f547cf3e
Jon Finerty <jonathanfinerty@gmail.com>
2022-02-20 11:10:00 +0000

    spacing

--- docs/js/poems.js
+++ docs/js/poems.js
@@ -331,26 +331,7 @@ It deepens like a coastal shelf.
 Get out as early as you can,
 And don’t have any kids yourself.
 
-
                     - Philip Larkin`,
-
-`Shall I compare thee to a summer’s day?
-Thou art more lovely and more temperate:
-Rough winds do shake the darling buds of May,
-And summer’s lease hath all too short a date;
-Sometime too hot the eye of heaven shines,
-And often is his gold complexion dimm'd;
-And every fair from fair sometime declines,
-By chance or nature’s changing course untrimm'd;
-But thy eternal summer shall not fade,
-Nor lose possession of that fair thou ow’st;
-Nor shall death brag thou wander’st in his shade,
-When in eternal lines to time thou grow’st:
-   So long as men can breathe or eyes can see,
-   So long lives this, and this gives life to thee.
-   
-
-                    - William Shakespeare`,
 
 `Double, double toil and trouble;
 Fire burn and caldron bubble.
@@ -367,7 +348,6 @@ Double, double toil and trouble;
 Fire burn and caldron bubble.
 Cool it with a baboon's blood,
 Then the charm is firm and good.
-
 
                     - William Shakespeare`,
 
