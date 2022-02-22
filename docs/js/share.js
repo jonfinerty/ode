40ada61a834386d4fe46bc14a685e29c438f894c
Jon Finerty <jonathanfinerty@gmail.com>
2022-02-22 09:10:03 +0000

    Add share fallback

--- docs/js/share.js
+++ docs/js/share.js
@@ -1,11 +1,10 @@
 "use strict";
 
-
 function shareClicked(event) {
     mixpanel.track('About clicked');
     event?.stopPropagation();
     hideRhymeSuggestions();
-  
+
     html2canvas(document.querySelector("#poem"))
       .then(canvas => {
         const img = canvas.toDataURL("image/png");
@@ -30,21 +29,81 @@ function shareClicked(event) {
   
   function shareImage(imageFile) {
   
-    let shareText = document.querySelector("#input").value.trimEnd();
-
-    shareText += ""
+    const poemText = document.querySelector("#input").value.trimEnd();
 
     const shareData = {
       files: [imageFile],
       title: getTitle(),
-      text: document.querySelector("#input").value
+      text: poemText
     }
   
-    // todo: override, desktop share is garbage
-    if (!navigator.canShare(shareData)) {
-      // can't native share image
-    }
-    else if (navigator.canShare(shareData)) {
+    if (supportsNativeShare(shareData)) {
       navigator.share(shareData);
+    } else {
+      if (navigator.clipboard) {
+        const data = [new ClipboardItem({ [imageFile.type]: imageFile })]
+        navigator.clipboard.write(data).then(function () {
+          // image copied to clipboard
+          setShareMessage("poem copied");
+        }, function (err) {
+          copyTextToClipboard(poemText);
+        })
+      } else {
+        fallbackCopyTextToClipboard(poemText);
+      }
     }
   }
+
+  function supportsNativeShare(shareData) {
+    const isMobileBrowser = navigator.userAgent.indexOf("Mobi") != -1;
+    return isMobileBrowser && navigator.canShare(shareData);
+  }
+
+function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+  
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    if (successful) {
+      setShareMessage("poem copied to clipboard");
+    }
+  } catch (err) {
+    console.error('Fallback: Oops, unable to copy', err);
+  }
+
+  document.body.removeChild(textArea);
+}
+
+function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+
+  navigator.clipboard.writeText(text).then(function() {
+    setShareMessage("poem copied to clipboard");
+  }, function(err) {
+    console.error('Async: Could not copy text: ', err);
+  });
+}
+
+function setShareMessage(message) {
+  const shareMenuItem = document.querySelector("#menu-item-share");
+  shareMenuItem.innerText = message;
+
+  setTimeout(() => {
+    const shareMenuItem = document.querySelector("#menu-item-share");
+    shareMenuItem.innerText = "share";    
+  }, 3000);
+}
